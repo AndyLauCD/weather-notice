e2ff78c632ac490156026d0f9dc48d68a94b476d
AndyLauCD <chendi@radi.ac.cn>
2019-11-05 23:29:21 +0800

    修改为配置化文件

---
+++ config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author:AndyLauCD                                                   
+# @Mail: y306118776@vip.qq.com                                        
+# @File: config.py                                                    
+# @Time: 2019/11/5 23:06                                              
+# @Desc: 对应的配置文件
+
+
+class ConfigWeatherWeChat(object):
+    key = ""
+    friend_city_name = []
+    friend_name = []
+    group_name = []
+    group_city_name = []
+    admin_chat_name = ""
--- parent-weather.py
+++ parent-weather.py
@@ -2,7 +2,7 @@ from wxpy import *
 import requests
 from threading import Timer
 from time import sleep
-
+from config import *
 
 """
 本文原创：pk哥
@@ -14,8 +14,8 @@ from time import sleep
 bot = Bot(cache_path=True)  # 扫码登录微信，如果在Linux环境中运行，加一个参数 bot = Bot(console_qr=-2,cache_path=True)
 
 
-def get_weather():
-    url = 'http://v.juhe.cn/weather/index?cityname=上海&key=xxx'  # 城市名cityname和key值换成自己的
+def get_weather(city, key):
+    url = 'http://v.juhe.cn/weather/index?cityname={}&key={}'.format(city, key)  # 城市名cityname和key值换成自己的
     weather_json = requests.get(url).json()
     temperature = weather_json['result']['today']['temperature']
     weather = weather_json['result']['today']['weather']
@@ -25,21 +25,29 @@ def get_weather():
     return temperature, weather, week, city, dressing_advice
 
 
-while True:   # ！！！调试时记得先把while True注释掉，不然会一直重复发送失败，一天限制100次调用的，成功后再加上注释
+# while True:   # ！！！调试时记得先把while True注释掉，不然会一直重复发送失败，一天限制100次调用的，成功后再加上注释
+if True:
     try:
-        temperature, weather, week, city, dressing_advice = get_weather()
-        # 发送到微信群里
-        my_groups = bot.groups().search('你的群名称')[0]   # 换成发送信息的群名称
-        msg = '今天是：' + week + '\n' \
-              + city + '的天气：' + weather + '\n' \
-              + '今天温度：' + temperature +'\n' \
-              + '穿衣指南：' + dressing_advice
-        print(msg)
-        my_groups.send(msg)
+        config = ConfigWeatherWeChat()
+        for i in range(len(config.group_city_name)):
+            city = config.group_city_name[i]
+            group_name = config.group_name[i]
+            temperature, weather, week, city, dressing_advice = get_weather(city, config.key)
+            # 发送到微信群里
+            my_groups = bot.groups().search(group_name)[0]   # 换成发送信息的群名称
+            msg = '今天是：' + week + '\n' \
+                  + city + '的天气：' + weather + '\n' \
+                  + '今天温度：' + temperature +'\n' \
+                  + '穿衣指南：' + dressing_advice
+            print(msg)
+            my_groups.send(msg)
 
         # 单独私发微信
-        # my_friend = bot.friends().search(u'pk')[0]  # 此处是对方自己的昵称，不是微信号，也不是你的备注。
-        # my_friend.send(msg)  # 发送文字
+        for i in range(len(config.friend_city_name)):
+            city = config.friend_city_name[i]
+            friend = config.friend_name[i]
+            my_friend = bot.friends().search(friend)[0]  # 此处是对方自己的昵称，不是微信号，也不是你的备注。
+            my_friend.send(msg)  # 发送文字
 
         t = Timer(86400, get_weather)  # Timer（定时器）是 Thread 的派生类，用于在指定时间后调用一个方法。
         t.start()
@@ -49,6 +57,6 @@ while True:   # ！！！调试时记得先把while True注释掉，不然会一
         my_friend = bot.friends().search(u'xxx')[
             0]  # 发送不成功，则发送消息给自己，提醒消息发送失败 xxx改成你自己微信的昵称
         my_friend.send(u'天气消息发送失败，请停止程序进行调试')
-        break
+        # break
 
 
